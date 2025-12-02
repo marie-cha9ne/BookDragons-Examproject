4c47fb8502aac1d3eb919b67f836ec86f61c6c34
marie-cha9ne <marie.c.eliassen@gokstadakademiet.no>
2025-12-02 17:36:06 +0100

    Removed shoppingcart from nav, added page navigation links #18

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,21 +1,44 @@
 "use client"
 import styles from './nav.module.css';
 import Image from 'next/image';
-import Cart from './assets/Shopping cart.svg';
+import Menu from './assets/Menu.svg';
+import X from './assets/X.svg';
+import { useState, useEffect } from 'react';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 
 export default function Nav(){
+  const [menuOpen, setMenuOpen] = useState(false);
 
+  const path = usePathname();
+
+  useEffect(()=>{
+    setMenuOpen(false)
+  }, [path])
   return(
     <nav className={styles.nav}>
     <h3 className={styles.title}>BookDragons</h3>
 
-    <div className={styles.cart}>
+    <ul 
+    className={`
+      ${styles.linkContainer}
+      ${menuOpen ? styles.open : ''}
+      `}>
+        <Link href={'/'} className={styles.homeLink}>Home</Link>
+        <Link href={'/'} className={styles.navLink}>Genres</Link>
+        <Link href={'/'} className={styles.navLink}>Authors</Link>
+    </ul>
+
+    <div 
+    className={styles.burgerMenu}
+    onClick={()=> setMenuOpen(!menuOpen)}
+    >
       <Image
-        src={Cart}
-        width={30}
+        src={menuOpen ? X : Menu}
+        width={40}
         height={40}
-        alt='Shopping cart'
+        alt='Navigation menu'
         />
     </div>
     </nav>
