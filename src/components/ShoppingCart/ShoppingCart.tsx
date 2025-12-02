5bad00a112dc996fa26c1cf1bbe77c9100dbda75
marie-cha9ne <marie.c.eliassen@gokstadakademiet.no>
2025-12-02 19:51:45 +0100

    Created zustand store for saving cart information #22

---
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -0,0 +1,2 @@
+"use client"
+import Cart from './assets/Shopping cart.svg';
---
+++ src/store/cartStore.ts
@@ -0,0 +1,43 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+type CartItem  ={
+  id:number,
+  title:string
+  price:number
+}
+
+type CartStore = {
+  cart: CartItem[],
+  addToCart: (item: CartItem) => void,
+  removeFromCart: (id: number) => void,
+  clearCart: () => void
+}
+
+export const useStoredCart = create<CartStore>()(
+  persist(
+    (set, get) => ({
+      cart: [],
+
+      addToCart(item) {
+       const oldCart = get().cart
+        const newList =  [...oldCart, item]
+        set({cart: newList})
+      },
+
+      removeFromCart(id) {
+        const oldCart = get().cart
+        const updatedCart = oldCart.filter((book) => book.id !== id)
+        set({cart: updatedCart});
+      },
+
+      clearCart: () => {
+        set({cart: []}) 
+      },
+
+    }),
+    {
+      name: 'storedCart',
+    }
+  )
+)
