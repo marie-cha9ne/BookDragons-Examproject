af09f602f40cddf65f814cc60308ef34fc50a9aa
marie-cha9ne <marie.c.eliassen@gokstadakademiet.no>
2025-12-02 13:33:49 +0100

    Created component for showcasing books, added card-structure and guards for the component

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,17 +1,25 @@
 import { getPayload } from 'payload'
 import config from '@/payload.config'
 import './styles.css'
-import payloadConfig from '@/payload.config'
+import payloadConfig from '@/payload.config';
+import BookCards from '@/components/BookCards/BookCards';
 
 export default async function HomePage() {
    const payload = await getPayload({config});
    const queryResults = await payload.find({
     collection:'books',
-    depth:2,
+    depth:1,
    })
+
+   const books = queryResults.docs;
   return (
     <div >
       <h1 className='title'>Book Dragons</h1>
+      {books.map((book) => (
+        <section key={book.id}>
+          <BookCards book={book}/>
+        </section>
+      ))}
     </div>
   )
 }
---
+++ src/components/BookCards/BookCards.tsx
@@ -0,0 +1,48 @@
+import type { Book } from '@/payload-types';
+import styles from './BookCards.module.css';
+import Image from 'next/image';
+
+type BookCardProps = {
+  book: Book,
+}
+
+export default async function BookCards({ book }: BookCardProps){
+
+  if(!book.bookCovers || typeof book.bookCovers !== 'object'){
+    return <p className={styles.errorMsg}>No image available for this book</p>
+  }
+
+  if(!book.author || typeof book.author !== 'object'){
+    return <p className={styles.errorMsg}>No author found</p>
+  }
+
+  if(!book.genre || typeof book.genre !== 'object'){
+    return <p className={styles.errorMsg}>No Genre found</p>
+  }
+
+return(
+  <section>
+    <div className={styles.cardContainer}>
+      <Image
+        src={book.bookCovers?.sizes?.thumbnail?.url ?? './fallback.jpg'}
+        alt={book.bookCovers.alt}
+        width={book.bookCovers?.sizes?.thumbnail?.width ?? 200}
+        height={book.bookCovers?.sizes?.thumbnail?.height ?? 200}
+        />
+      <h2>{book.title}</h2>
+      <div className={styles.bookInfo}>
+        <p>Books available: {book.availablilty}</p>
+        <p>Age recommendation: <strong>{book.ageRec}</strong></p>
+      </div>
+      {/* Later: add dialog, details or dynamicrouting to author and genre */}
+      <p>Author of the book: {book.author.name}</p>
+      <p>Book genre: {book.genre.genre}</p>
+
+      <p>Price: <strong>{book.price}</strong>,-</p>
+
+      <button>Add to cart</button>
+    </div>
+  </section>
+)
+
+}
