43246df324af64f41ed7a8aca4bf77f9b7c1fff4
marie-cha9ne <marie.c.eliassen@gokstadakademiet.no>
2025-12-05 14:02:14 +0100

    Error message from parload solved, edited orders-collection, shoppingCart and customerForm got updated logic, aswell as some modifications to the carts UI #48

--- src/app/api/custom-orders/route.ts
+++ src/app/api/custom-orders/route.ts
@@ -6,15 +6,28 @@ export async function POST(req: Request){
   const payload = await getPayload({ config });
   const data = await req.json();
 
+  // -- Sanity checks --
+  // sjekk 1: navn:
   if(!data.customerName){
     return NextResponse.json({error: 'Missing customer name'}, {status: 400})
   };
+
+  // sjekk 2: kontaktinfo
   if(!data.customerInfo){
     return NextResponse.json({error:'Missing customer info mail/phone'}, {status: 400})
   };
-  if(!Array.isArray(data.reserveBook) || data.reserveBook.length === 0){
+
+  // sjekk 3: minst 1 vare i items[]
+  if(!Array.isArray(data.items) || data.items.length === 0){
     return NextResponse.json({error: 'No books selected'}, {status: 400})
   };
+
+  // sjekk 4: alle items er i riktig format
+  for(const item of data.items){
+    if(!item.book || !item.bookQuantity){
+      return NextResponse.json({error:'invalid format'}, {status:400})
+    }
+  }
 
   const order = await payload.create({
     collection: 'orders',
